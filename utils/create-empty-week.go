b7bfac09fe78b9989e49f7aa62fff3e5683f5eeb
Andrzej Lech <andrzejdlech@gmail.com>
2022-10-20 18:28:36 +0200

    add hours to empty subjects

--- utils/create-empty-week.go
+++ utils/create-empty-week.go
@@ -8,11 +8,28 @@ func CreateEmptyWeek() Week {
 		Name:     "-",
 		Lecturer: "-",
 		Room:     "-",
-		}
-		var emptySubjects []Subject
+	}
+	var emptySubjects []Subject
 
 	m := 0
 	for m <= 6 {
+		switch m {
+		case 0:
+			emptySubject.Hour = "08:15-09:45"
+		case 1:
+			emptySubject.Hour = "10:00-11:30"
+		case 2:
+			emptySubject.Hour = "11:45-13:15"
+		case 3:
+			emptySubject.Hour = "13:30-15:00"
+		case 4:
+			emptySubject.Hour = "15:15-16:45"
+		case 5:
+			emptySubject.Hour = "17:00-18:30"
+		case 6:
+			emptySubject.Hour = "18:45-20:15"
+		}
+		
 		emptySubjects = append(emptySubjects, emptySubject)
 		m++
 	}
@@ -24,7 +41,7 @@ func CreateEmptyWeek() Week {
 		week.Days = append(week.Days, Day{
 			Name:     "-",
 			Subjects: emptySubjects,
-			})
+		})
 		n++
 	}
 
