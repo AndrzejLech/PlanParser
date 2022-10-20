cb25c9d17dd758c7dbef25794f6748d16490abb5
Andrzej Lech <andrzejdlech@gmail.com>
2022-10-20 16:06:27 +0200

    fix week starting day

--- controllers/mfz-controller.go
+++ controllers/mfz-controller.go
@@ -33,16 +33,22 @@ func GetMFZ12PerWeek(context *gin.Context) {
 		newWeekday = GetWeekdaysStraight(newWeekday)
 
 		fmt.Print(
+			"date: " + strconv.Itoa(previousDate.Day()) +
+				" weekday: " + ChangeWeekdayNumberToName(previousWeekday) +
+				" start week: " + strconv.Itoa(previousDate.AddDate(0, 0, -(previousWeekday)).Day()) + "\n",
+		)
+		fmt.Print(
 			"date: " + strconv.Itoa(date.Day()) +
 				" weekday: " + ChangeWeekdayNumberToName(newWeekday) +
 				" start week: " + strconv.Itoa(date.AddDate(0, 0, -(newWeekday)).Day()) + "\n",
 		)
 
-		if date.AddDate(0, 0, -newWeekday).Day() == previousDate.AddDate(0,0, -previousWeekday).Day() {
-			week.Days[newWeekday-1] = days[index]
+		if date.AddDate(0, 0, -newWeekday).Day() == previousDate.AddDate(0, 0, -previousWeekday).Day() || index == 0 {
+			week.Days[newWeekday] = days[index]
 		} else {
 			weeks = append(weeks, week)
 			week = CreateEmptyWeek()
+			week.Days[newWeekday] = days[index]
 		}
 
 		previousDate = date
